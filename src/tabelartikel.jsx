b61fb9513edae77e3deb1678abff703b2c8279f2
Nazwa Septya Salsabilla <nazwasalsabila1002@gmail.com>
2023-07-19 13:08:51 +0700

    tabel

--- src/tabelartikel.jsx
+++ src/tabelartikel.jsx
@@ -1,14 +1,173 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from './layout';
-import { Link } from 'react-router-dom';
+import DataTable from 'react-data-table-component';
+import Modal from 'react-modal';
 
-const Tabelartikel = () => {
+const EditButton = ({ row, onEdit }) => {
+  const handleEdit = () => {
+    onEdit(row);
+  };
+
+  const editButtonStyle = {
+    padding: '8px 12px',
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    width: '80px',
+    fontSize: '10px',
+  };
+
+  return (
+    <button onClick={handleEdit} style={editButtonStyle}>
+      Edit
+    </button>
+  );
+};
+
+const DeleteButton = ({ row, onDelete }) => {
+  const handleDelete = () => {
+    onDelete(row);
+  };
+
+  const deleteButtonStyle = {
+    padding: '8px 10px',
+    backgroundColor: '#f44336',
+    color: 'white',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    width: '80px',
+    fontSize: '10px',
+  };
+
+  return (
+    <button onClick={handleDelete} style={deleteButtonStyle}>
+      Hapus
+    </button>
+  );
+};
+
+const TabelArtikel = () => {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      judul: 'Cara Menulis Artikel yang Menarik',
+      penulis: 'John Doe',
+      tanggal: '2023-07-19',
+      kategori: 'Tips & Tricks',
+      isi: 'Ini adalah contoh isi artikel yang menarik...',
+      gambar: 'https://example.com/gambar_artikel_1.jpg',
+    },
+    {
+      id: 2,
+      judul: 'Pengenalan React Hooks',
+      penulis: 'Jane Doe',
+      tanggal: '2023-07-20',
+      kategori: 'Programming',
+      isi: 'React Hooks adalah fitur terbaru dari React...',
+      gambar: 'https://example.com/gambar_artikel_2.jpg',
+    },
+    // Add more data objects as needed
+  ]);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedRow, setEditedRow] = useState(null);
+
+  // Function to generate a new ID
+  const generateNewId = () => {
+    const maxId = data.reduce((max, item) => Math.max(max, item.id), 0);
+    return maxId + 1;
+  };
+
+  // Function to handle the edit action
+  const handleEdit = (row) => {
+    setEditedRow(row);
+    setIsEditing(true);
+  };
+
+  // Function to handle the delete action
+  const handleDelete = (row) => {
+    setData((prevData) => prevData.filter((item) => item.id !== row.id));
+    // Setelah menghapus data, kita perlu menyesuaikan kembali nomor ID
+    // agar tetap konsisten dan mengikuti urutan data yang ada
+    setData((prevData) =>
+      prevData.map((item, index) => ({ ...item, id: index + 1 })),
+    );
+  };
+
+  const handleSave = () => {
+    setData((prevData) =>
+      prevData.map((item, index) => ({ ...item, id: index + 1 })),
+    );
+    setIsEditing(false);
+    setEditedRow(null);
+  };
+
+  const handleCloseModal = () => {
+    setIsEditing(false);
+    setEditedRow(null);
+  };
+
+  const columns = [
+    {
+      name: 'ID',
+      selector: 'id',
+      maxWidth: '70px', // Set the maximum width of the ID column
+      cell: (row) => <span style={{ fontWeight: 'bold' }}>{row.id}</span>,
+    },
+    {
+      name: 'Judul',
+      selector: 'judul',
+    },
+    {
+      name: 'Penulis',
+      selector: 'penulis',
+    },
+    {
+      name: 'Tanggal',
+      selector: 'tanggal',
+    },
+    {
+      name: 'Kategori',
+      selector: 'kategori',
+    },
+    {
+      name: 'Isi Artikel',
+      selector: 'isi',
+      wrap: true, // Wrap the content to fit in the column width
+    },
+    {
+      name: 'Gambar Artikel',
+      cell: (row) => (
+        <img
+          src={row.gambar}
+          alt={row.judul}
+          style={{ maxWidth: '100px', maxHeight: '100px' }}
+        />
+      ),
+      allowOverflow: true,
+    },
+    {
+      name: 'Actions',
+      cell: (row) => (
+        <>
+          <EditButton row={row} onEdit={handleEdit} />
+          <DeleteButton row={row} onDelete={handleDelete} />
+        </>
+      ),
+      ignoreRowClick: true,
+      allowOverflow: true,
+      button: true,
+    },
+  ];
+
   return (
     <>
       <Layout>
         <div className="page-content">
           <div className="container-fluid">
-            {/* start page title */}
             <div className="row">
               <div className="col-12">
                 <div className="page-title-box d-flex align-items-center justify-content-between">
@@ -16,134 +175,129 @@ const Tabelartikel = () => {
                 </div>
               </div>
             </div>
-            {/* end page title */}
-            <div className="row">
-              <div className="col-lg-12">
-                <div className="card">
-                  <div className="card-body">
-                    <div>
-                      <div className="float-end">
-                        <form className="d-inline-flex mb-3">
-                          <label
-                            className="form-check-label my-2 me-2"
-                            htmlFor="order-selectinput"
-                          >
-                            Kategori
-                          </label>
-                          <select
-                            className="form-select"
-                            id="order-selectinput"
-                          >
-                            <option selected="">All</option>
-                            <option value={1}>Industri dan Organisasi</option>
-                            <option value={2}>Karir</option>
-                            <option value={3}>Keluarga</option>
-                            <option value={4}>Masalah Diri</option>
-                            <option value={5}>Trauma</option>
-                          </select>
-                        </form>
-                      </div>
-                      <button
-                        type="button"
-                        className="btn btn-success waves-effect waves-light mb-3"
-                      >
-                        <i className="mdi mdi-plus me-1" /> Add New Artikel
-                      </button>
-                    </div>
-                    <div className="table-responsive mb-4">
-                      <table
-                        className="table table-centered datatable dt-responsive nowrap table-card-list"
-                        style={{
-                          borderCollapse: 'collapse',
-                          width: '100%',
-                        }}
-                      >
-                        <thead>
-                          <tr>
-                            <th style={{ width: 20 }}>
-                              <div className="form-check text-center font-size-16">
-                                <input
-                                  type="checkbox"
-                                  className="form-check-input"
-                                  id="ordercheck"
-                                />
-                                <label
-                                  className="form-check-label"
-                                  htmlFor="ordercheck"
-                                />
-                              </div>
-                            </th>
-                            <th>ID</th>
-                            <th>Banner Artikel</th>
-                            <th>Tanggal</th>
-                            <th>Isi Artikel</th>
-                            <th>Nama Penulis</th>
-                            <th style={{ width: 120 }}>Action</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>
-                              <div className="form-check text-center font-size-16">
-                                <input
-                                  type="checkbox"
-                                  className="form-check-input"
-                                  id="ordercheck1"
-                                />
-                                <label
-                                  className="form-check-label"
-                                  htmlFor="ordercheck1"
-                                />
-                              </div>
-                            </td>
-                            <td>
-                              <a
-                                href="javascript: void(0);"
-                                className="text-reset  fw-bold"
-                              >
-                                1
-                              </a>{' '}
-                            </td>
-                            <td>banner.png</td>
-                            <td>05 May 2023</td>
-                            <td>
-                              Sed perspicia unde omnis iste natus error
-                              voluptatem accsam dolor laudantium waset
-                              totamaperiam eaqipsaty quaenventore veritatis
-                              quasi architecto beatae
-                            </td>
-                            <td>Budi</td>
-                            <td>
-                              <a
-                                href="javascript:void(0);"
-                                className="px-3 text-primary"
-                              >
-                                <i className="uil uil-pen font-size-18" />
-                              </a>
-                              <a
-                                href="javascript:void(0);"
-                                className="px-3 text-danger"
-                              >
-                                <i className="uil uil-trash-alt font-size-18" />
-                              </a>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                    {/* end table */}
-                  </div>
-                  {/* end row */}
-                </div>{' '}
-                {/* container-fluid */}
-              </div>
-              {/* End Page-content */}
+            <div className="container mt-5">
+              <DataTable columns={columns} data={data} pagination />
             </div>
           </div>
         </div>
       </Layout>
+      <div className="container mt-5">
+        <Modal
+          isOpen={isEditing}
+          onRequestClose={handleCloseModal}
+          className="modal-dialog container mt-5"
+          style={{
+            content: {
+              maxWidth: '600px', // Set the maximum width of the modal
+              margin: '0 auto', // Center the modal horizontally
+              border: '2px solid #4CAF50', // Add a border with green color
+              borderRadius: '8px', // Add border radius to make it look better
+              background: 'white', // Set the background color to white
+              transform: 'translate(0, 10%)', // Move it 50% from the left and 50% from the top
+            },
+            overlay: {
+              backgroundColor: 'rgba(0, 0, 0, 0.5)', // Add overlay background color
+            },
+          }}
+        >
+          <div className="modal-content">
+            <div className="modal-header">
+              <h2>Edit Data Artikel</h2>
+              <button
+                type="button"
+                className="btn-close"
+                onClick={handleCloseModal}
+              ></button>
+            </div>
+            {editedRow && (
+              <div className="modal-body">
+                <label>Judul:</label>
+                <input
+                  type="text"
+                  value={editedRow.judul}
+                  onChange={(e) =>
+                    setEditedRow({ ...editedRow, judul: e.target.value })
+                  }
+                  className="form-control"
+                />
+                <label>Penulis:</label>
+                <input
+                  type="text"
+                  value={editedRow.penulis}
+                  onChange={(e) =>
+                    setEditedRow({ ...editedRow, penulis: e.target.value })
+                  }
+                  className="form-control"
+                />
+                <label>Tanggal:</label>
+                <input
+                  type="date"
+                  value={editedRow.tanggal}
+                  onChange={(e) =>
+                    setEditedRow({ ...editedRow, tanggal: e.target.value })
+                  }
+                  className="form-control"
+                />
+                <label>Kategori:</label>
+                <input
+                  type="text"
+                  value={editedRow.kategori}
+                  onChange={(e) =>
+                    setEditedRow({ ...editedRow, kategori: e.target.value })
+                  }
+                  className="form-control"
+                />
+                <label>Isi Artikel:</label>
+                <textarea
+                  value={editedRow.isi}
+                  onChange={(e) =>
+                    setEditedRow({ ...editedRow, isi: e.target.value })
+                  }
+                  className="form-control"
+                />
+                <label>Gambar Artikel:</label>
+                <input
+                  type="text"
+                  value={editedRow.gambar}
+                  onChange={(e) =>
+                    setEditedRow({ ...editedRow, gambar: e.target.value })
+                  }
+                  className="form-control"
+                />
+              </div>
+            )}
+            <div
+              className="modal-footer"
+              style={{ marginTop: '10px', marginBottom: '10px' }}
+            >
+              <button
+                className="btn btn-primary"
+                onClick={handleSave}
+                style={{
+                  backgroundColor: '#4CAF50',
+                  marginRight: '10px',
+                  border: '2px solid #4CAF50',
+                }}
+              >
+                Save
+              </button>
+              <button
+                className="btn btn-secondary"
+                onClick={handleCloseModal}
+                style={{
+                  backgroundColor: '#f44336',
+                  marginRight: '10px',
+                  border: '2px solid #f44336',
+                }}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </Modal>
+      </div>
     </>
   );
 };
 
-export default Tabelartikel;
+export default TabelArtikel;
--- src/tabelkonsultasi.jsx
+++ src/tabelkonsultasi.jsx
@@ -1,14 +1,171 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from './layout';
-import { Link } from 'react-router-dom';
+import DataTable from 'react-data-table-component';
+import Modal from 'react-modal';
+
+const EditButton = ({ row, onEdit }) => {
+  const handleEdit = () => {
+    onEdit(row);
+  };
+
+  const editButtonStyle = {
+    padding: '8px 12px',
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    width: '80px',
+    fontSize: '10px',
+  };
+
+  return (
+    <button onClick={handleEdit} style={editButtonStyle}>
+      Edit
+    </button>
+  );
+};
+
+const DeleteButton = ({ row, onDelete }) => {
+  const handleDelete = () => {
+    onDelete(row);
+  };
+
+  const deleteButtonStyle = {
+    padding: '8px 10px',
+    backgroundColor: '#f44336',
+    color: 'white',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    width: '80px',
+    fontSize: '10px',
+  };
+
+  return (
+    <button onClick={handleDelete} style={deleteButtonStyle}>
+      Hapus
+    </button>
+  );
+};
 
 const Tabelkonsultasi = () => {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      nama: 'John Doe',
+      email: 'john@example.com',
+      tanggal: '2023-07-19',
+      nohp: '123456789',
+      psikolog: 'Nadjillah Fatin',
+      alamat: '123 Main Street',
+      keluhan: 'Anxiety',
+    },
+    {
+      id: 2,
+      nama: 'Jane Doe',
+      email: 'jane@example.com',
+      tanggal: '2023-07-20',
+      nohp: '987654321',
+      psikolog: 'Nadjillah Fatin',
+      alamat: '456 Oak Avenue',
+      keluhan: 'Depression',
+    },
+    // Add more data objects as needed
+  ]);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedRow, setEditedRow] = useState(null);
+
+  // Function to generate a new ID
+  const generateNewId = () => {
+    const maxId = data.reduce((max, item) => Math.max(max, item.id), 0);
+    return maxId + 1;
+  };
+
+  // Function to handle the edit action
+  const handleEdit = (row) => {
+    setEditedRow(row);
+    setIsEditing(true);
+  };
+
+  // Function to handle the delete action
+  const handleDelete = (row) => {
+    setData((prevData) => prevData.filter((item) => item.id !== row.id));
+    // Setelah menghapus data, kita perlu menyesuaikan kembali nomor ID
+    // agar tetap konsisten dan mengikuti urutan data yang ada
+    setData((prevData) =>
+      prevData.map((item, index) => ({ ...item, id: index + 1 })),
+    );
+  };
+
+  const handleSave = () => {
+    setData((prevData) =>
+      prevData.map((item, index) => ({ ...item, id: index + 1 })),
+    );
+    setIsEditing(false);
+    setEditedRow(null);
+  };
+
+  const handleCloseModal = () => {
+    setIsEditing(false);
+    setEditedRow(null);
+  };
+
+  const columns = [
+    {
+      name: 'ID',
+      selector: 'id',
+      maxWidth: '70px', // Set the maximum width of the ID column
+      cell: (row) => <span style={{ fontWeight: 'bold' }}>{row.id}</span>,
+    },
+    {
+      name: 'Nama',
+      selector: 'nama',
+    },
+    {
+      name: 'Email',
+      selector: 'email',
+    },
+    {
+      name: 'Tanggal',
+      selector: 'tanggal',
+    },
+    {
+      name: 'No. HP',
+      selector: 'nohp',
+    },
+    {
+      name: 'Psikolog',
+      selector: 'psikolog',
+    },
+    {
+      name: 'Alamat',
+      selector: 'alamat',
+    },
+    {
+      name: 'Keluhan',
+      selector: 'keluhan',
+    },
+    {
+      name: 'Actions',
+      cell: (row) => (
+        <>
+          <EditButton row={row} onEdit={handleEdit} />
+          <DeleteButton row={row} onDelete={handleDelete} />
+        </>
+      ),
+      ignoreRowClick: true,
+      allowOverflow: true,
+      button: true,
+    },
+  ];
+
   return (
     <>
       <Layout>
         <div className="page-content">
           <div className="container-fluid">
-            {/* start page title */}
             <div className="row">
               <div className="col-12">
                 <div className="page-title-box d-flex align-items-center justify-content-between">
@@ -16,106 +173,135 @@ const Tabelkonsultasi = () => {
                 </div>
               </div>
             </div>
-            {/* end page title */}
-            <div className="row">
-              <div className="col-lg-12">
-                <div className="card">
-                  <div className="card-body">
-                    <div>
-                      <div className="float-end"></div>
-                    </div>
-                    <div className="table-responsive mb-4">
-                      <table
-                        className="table table-centered datatable dt-responsive nowrap table-card-list"
-                        style={{
-                          borderCollapse: 'collapse',
-                          width: '100%',
-                        }}
-                      >
-                        <thead>
-                          <tr>
-                            <th style={{ width: 20 }}>
-                              <div className="form-check text-center font-size-16">
-                                <input
-                                  type="checkbox"
-                                  className="form-check-input"
-                                  id="ordercheck"
-                                />
-                                <label
-                                  className="form-check-label"
-                                  htmlFor="ordercheck"
-                                />
-                              </div>
-                            </th>
-
-                            <th>Nama</th>
-                            <th>Email</th>
-                            <th>Tanggal</th>
-                            <th>No.HP</th>
-                            <th>Psikolog</th>
-                            <th>Alamat</th>
-                            <th>Keluhan</th>
-                            <th style={{ width: 120 }}>Action</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>
-                              <div className="form-check text-center font-size-16">
-                                <input
-                                  type="checkbox"
-                                  className="form-check-input"
-                                  id="ordercheck1"
-                                />
-                                <label
-                                  className="form-check-label"
-                                  htmlFor="ordercheck1"
-                                />
-                              </div>
-                            </td>
-                            <td>
-                              <a
-                                href="javascript: void(0);"
-                                className="text-reset  fw-bold"
-                              >
-                                Budi
-                              </a>{' '}
-                            </td>
-                            <td>email@gmail.com</td>
-                            <td>05 May 2023</td>
-                            <td>08123456789</td>
-                            <td>Nadjillah Fatin</td>
-                            <td>Jl. persatuan</td>
-                            <td>Sakit</td>
-                            <td>
-                              <a
-                                href="javascript:void(0);"
-                                className="px-3 text-primary"
-                              >
-                                <i className="uil uil-pen font-size-18" />
-                              </a>
-                              <a
-                                href="javascript:void(0);"
-                                className="px-3 text-danger"
-                              >
-                                <i className="uil uil-trash-alt font-size-18" />
-                              </a>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                    {/* end table */}
-                  </div>
-                  {/* end row */}
-                </div>{' '}
-                {/* container-fluid */}
-              </div>
-              {/* End Page-content */}
+            <div className="container mt-5">
+              <DataTable columns={columns} data={data} pagination />
             </div>
           </div>
         </div>
       </Layout>
+      <div className="container mt-5">
+        <Modal
+          isOpen={isEditing}
+          onRequestClose={handleCloseModal}
+          className="modal-dialog container mt-5"
+          style={{
+            content: {
+              maxWidth: '400px', // Set the maximum width of the modal
+              margin: '0 auto', // Center the modal horizontally
+              border: '2px solid #ffffff', // Add a border with green color
+              borderRadius: '8px', // Add border radius to make it look better
+              background: 'white', // Set the background color to white
+              transform: 'translate(0, 10%)', // Move it 50% from the left and 50% from the top
+            },
+            overlay: {
+              backgroundColor: 'rgba(0, 0, 0, 0.5)', // Add overlay background color
+            },
+          }}
+        >
+          <div className="modal-content">
+            <div className="modal-header">
+              <h2>Edit Data</h2>
+              <button
+                type="button"
+                className="btn-close"
+                onClick={handleCloseModal}
+              ></button>
+            </div>
+            {editedRow && (
+              <div className="modal-body">
+                <label>Nama:</label>
+                <input
+                  type="text"
+                  value={editedRow.nama}
+                  onChange={(e) =>
+                    setEditedRow({ ...editedRow, nama: e.target.value })
+                  }
+                  className="form-control"
+                />
+                <label>Email:</label>
+                <input
+                  type="text"
+                  value={editedRow.email}
+                  onChange={(e) =>
+                    setEditedRow({ ...editedRow, email: e.target.value })
+                  }
+                  className="form-control"
+                />
+                <label>Tanggal:</label>
+                <input
+                  type="date"
+                  value={editedRow.tanggal}
+                  onChange={(e) =>
+                    setEditedRow({ ...editedRow, tanggal: e.target.value })
+                  }
+                  className="form-control"
+                />
+                <label>No. HP:</label>
+                <input
+                  type="text"
+                  value={editedRow.nohp}
+                  onChange={(e) =>
+                    setEditedRow({ ...editedRow, nohp: e.target.value })
+                  }
+                  className="form-control"
+                />
+                <label>Psikolog:</label>
+                <input
+                  type="text"
+                  value={editedRow.psikolog}
+                  readOnly // Set the input as read-only
+                  className="form-control"
+                />
+                <label>Alamat:</label>
+                <input
+                  type="text"
+                  value={editedRow.alamat}
+                  onChange={(e) =>
+                    setEditedRow({ ...editedRow, alamat: e.target.value })
+                  }
+                  className="form-control"
+                />
+                <label>Keluhan:</label>
+                <input
+                  type="text"
+                  value={editedRow.keluhan}
+                  onChange={(e) =>
+                    setEditedRow({ ...editedRow, keluhan: e.target.value })
+                  }
+                  className="form-control"
+                />
+              </div>
+            )}
+            <div
+              className="modal-footer"
+              style={{ marginTop: '10px', marginBottom: '10px' }}
+            >
+              <button
+                className="btn btn-primary"
+                onClick={handleSave}
+                style={{
+                  backgroundColor: '#4CAF50',
+                  marginRight: '10px',
+                  border: '2px solid #4CAF50',
+                }}
+              >
+                Save
+              </button>
+              <button
+                className="btn btn-secondary"
+                onClick={handleCloseModal}
+                style={{
+                  backgroundColor: '#f44336',
+                  marginRight: '10px',
+                  border: '2px solid #f44336',
+                }}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </Modal>
+      </div>
     </>
   );
 };
